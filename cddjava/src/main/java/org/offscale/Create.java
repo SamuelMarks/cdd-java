0158cecb66784fe03b669399e6d8ba88d2fa8f30
skushnir123 <samkushnir@gmail.com>
2022-09-08 09:29:15 -0400

    Creation of Parameter class

--- cddjava/src/main/java/org/offscale/Create.java
+++ cddjava/src/main/java/org/offscale/Create.java
@@ -6,30 +6,63 @@ import com.github.javaparser.ast.expr.NormalAnnotationExpr;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.yaml.snakeyaml.Yaml;
+
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * Given an OpenAPI Spec, generates code corresponding to the schemas and paths
+ * Given an OpenAPI Spec, generates code for routes and components.
  */
 public class Create {
     private final JSONObject jo;
     private static final ImmutableMap<String, String> OPEN_API_TO_JAVA = Utils.getOpenAPIToJavaTypes();
 
+    private class Parameter {
+        private String type;
+        private String name;
+
+        private String description;
+
+        private String strictType;
+
+        public Parameter (String type) {
+            this.type = type;
+        }
+
+        public Parameter(String type, String strictType) {
+            this.type = type;
+            this.strictType = strictType;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
+
     public Create(String filePath) {
         this.jo = getJSONObjectFromFile(filePath);
-        generateClasses();
+        generateComponents();
         generateRoutes();
     }
 
     /**
-     * Given a filePath to a yaml file, returns a corresponding JSONObject
-     *
      * @param filePath of yaml file
+     * @return JSONObject corresponding to openAPI spec from yaml file
      */
     public JSONObject getJSONObjectFromFile(String filePath) {
         Yaml yaml = new Yaml();
@@ -41,19 +74,18 @@ public class Create {
     }
 
     /**
-     * Generates the classes corresponding to the schemas in the JSON Object
+     * Generates the classes corresponding to the schemas in the OpenAPI spec
      */
-    public ImmutableList<String> generateClasses() {
-        List<String> generatedClasses = new ArrayList<>();
+    public ImmutableList<String> generateComponents() {
+        List<String> generatedComponents = new ArrayList<>();
         JSONObject joSchemas = jo.getJSONObject("components").getJSONObject("schemas");
         List<String> schemas = Lists.newArrayList(joSchemas.keys());
-        schemas.forEach((schema) -> generatedClasses.add(generateClass(joSchemas.getJSONObject(schema), schema)));
-        return ImmutableList.copyOf(generatedClasses);
+        schemas.forEach((schema) -> generatedComponents.add(generateComponent(joSchemas.getJSONObject(schema), schema)));
+        return ImmutableList.copyOf(generatedComponents);
     }
 
     /**
-     *
-     * @return Routes interface which contains all the routes in the JSONObject
+     * @return Routes interface containing routes from OpenAPI Spec
      */
     public String generateRoutes() {
         JSONObject joPaths = jo.getJSONObject("paths");
@@ -70,6 +102,13 @@ public class Create {
         return myInterface.toString();
     }
 
+    /**
+     *
+     * @param routesInterface
+     * @param joRoute
+     * @param routeName
+     * @param operation
+     */
     private void generateRoute(ClassOrInterfaceDeclaration routesInterface, JSONObject joRoute, String routeName, String operation) {
         MethodDeclaration methodDeclaration = routesInterface.addMethod(joRoute.getString("operationId"));
         methodDeclaration.setJavadocComment(generateJavadocForRoute(joRoute, routeName, operation));
@@ -77,25 +116,82 @@ public class Create {
         expr.addPair("path", "\"" + routeName + "\"");
         methodDeclaration.removeBody();
         methodDeclaration.setType(generateRouteType(joRoute.getJSONObject("responses")));
+        if (joRoute.has("parameters")) {
+            generateRouteParameters(joRoute.getJSONArray("parameters")).forEach(param -> methodDeclaration.addParameter(param.type, param.name));
+        }
     }
 
+    /**
+     *
+     * @param joRouteParameters the openAPI representation of the route parameters
+     * @return a List of route parameters
+     */
+    private List<Parameter> generateRouteParameters(JSONArray joRouteParameters) {
+        List<Parameter> routeParameters = new ArrayList<>();
+        for (int i = 0; i < joRouteParameters.length(); i++) {
+            JSONObject joParameter = joRouteParameters.getJSONObject(i);
+            Parameter parameter = parseSchema(joParameter.getJSONObject("schema"));
+            parameter.setName(joParameter.getString("name"));
+            parameter.setDescription(joParameter.getString("description"));
+            routeParameters.add(parameter);
+        }
+        return routeParameters;
+    }
+
+    /**
+     * Generates the return type of a route
+     * @param joRouteResponse the OpenAPI representation of the route response.
+     * @return the type of the route
+     */
     private String generateRouteType(JSONObject joRouteResponse) {
         StringBuilder type = new StringBuilder();
         List<String> responses = Lists.newArrayList(joRouteResponse.keys());
         responses.forEach(response -> {
             if (!response.equals("default")) {
                 if (joRouteResponse.getJSONObject(response).has("content")) {
-
-                } else {
-                    type.append("void");
+                    Parameter parameter = parseSchema(joRouteResponse.getJSONObject(response).getJSONObject("content").getJSONObject("application/json").getJSONObject("schema"));
+                    parameter.setType("Call<" + parameter.type + ">");
+                    type.append(parameter.type);
                 }
             }
         });
-        return type.toString();
+        if (type.isEmpty()) {
+            return "void";
+        } else {
+            return type.toString();
+        }
     }
 
     /**
      *
+     * @param schema which is essentially a type
+     * @return a Parameter with type information
+     */
+    private Parameter parseSchema(JSONObject schema) {
+        if (schema.has("$ref")) {
+            return new Parameter(parseSchemaRef(schema.getString("$ref")));
+        }
+
+        if (schema.has("format")) {
+            return new Parameter(Utils.getOpenAPIToJavaTypes().get(schema.get("format")), schema.getString("format"));
+        }
+
+        return new Parameter(Utils.getOpenAPIToJavaTypes().get(schema.get("type")), schema.getString("type"));
+    }
+
+    /**
+     * Uses regex to parse out the component name in the reference.
+     * @param ref of a schema, maps to a component
+     * @return the component name
+     */
+    private String parseSchemaRef(String ref) {
+        Pattern pattern = Pattern.compile("#/components/schemas/(\\w+)");
+        Matcher matcher = pattern.matcher(ref);
+        matcher.find();
+        return matcher.group(1);
+    }
+
+    /**
      * @param joRoute
      * @param routeName
      * @param operation
@@ -106,10 +202,10 @@ public class Create {
         List<String> responses = Lists.newArrayList(joResponses.keys());
         StringBuilder javaDocForRoute = new StringBuilder(joRoute.getString("summary") + "\n");
         if (joRoute.has("parameters")) {
-            for (int i = 0; i < joRoute.getJSONArray("parameters").length(); i++) {
-                String param = "@param " + joRoute.getJSONArray("parameters").getJSONObject(i).getString("name") + "\n";
+            generateRouteParameters(joRoute.getJSONArray("parameters")).forEach(parameter -> {
+                String param = "@param " + parameter.name + " of type " + parameter.strictType + ". " + parameter.description + ". \n";
                 javaDocForRoute.append(param);
-            }
+            });
         }
         javaDocForRoute.append("@return ");
         responses.forEach((response) -> javaDocForRoute.append(generateJavadocReturn(joResponses.getJSONObject(response), response)));
@@ -122,20 +218,18 @@ public class Create {
     }
 
     /**
-     * Given a JSON Object and the name of a class, generate a new class with properties defined in the JSON Object
+     * Generates code for a component.
+     * @param joComponent JSONObject for a component in the OpenAPI spec.
+     * @param componentName name of the component to generate.
+     * @return a String containing the generated code for a component.
      */
-    private String generateClass(JSONObject joClass, String className) {
-        JSONObject joProperties = joClass.getJSONObject("properties");
+    private String generateComponent(JSONObject joComponent, String componentName) {
+        JSONObject joProperties = joComponent.getJSONObject("properties");
         List<String> properties = Lists.newArrayList(joProperties.keys());
-        ClassOrInterfaceDeclaration myClass = new ClassOrInterfaceDeclaration();
+        ClassOrInterfaceDeclaration myComponent = new ClassOrInterfaceDeclaration();
 
-        myClass.setName(className);
-        // TODO: handle int64 and int32 differently
-        properties.forEach((property) -> myClass.addField(OPEN_API_TO_JAVA.get(joProperties.getJSONObject(property).getString("type")), property));
-        myClass.setJavadocComment("Howdy, gents?\n@param name of child");
-        System.out.println(myClass);
-        return myClass.toString();
+        myComponent.setName(componentName);
+        properties.forEach((property) -> myComponent.addField(OPEN_API_TO_JAVA.get(joProperties.getJSONObject(property).getString("type")), property));
+        return myComponent.toString();
     }
-
-
 }
